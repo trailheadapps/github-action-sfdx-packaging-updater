e1d53f4c0a8640cc72604c732067c602d5b3ad92
muenzpraeger <mail@winkelmeyer.com>
2019-11-30 20:14:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# sfdx-packaging-updater
+
+This Github Action is a helper action to extract revmoe not needed package versions from the `sfdx-project.json` file. It also updates any references in the README to the latest available package version.
+
+## Outputs
+
+This action provides one output:
+
+-   `isSuccess` - true if the result status code equals 0.
+
+## Example
+
+```yml
+# Update sfdx-project.json and README to use only latest package version
+- name: 'Extract package:version:create result data'
+  id: packaging-updater
+  uses: muenzpraeger/github-action-sfdx-packaging-updater
+```
---
+++ index.js
@@ -0,0 +1,59 @@
+const core = require('@actions/core');
+const fs = require('fs');
+const path = process.cwd();
+const sfdxJson = require(path + '/sfdx-project.json');
+
+async function run() {
+    try {
+        // Reading the content of the README file
+        let readmeContent = fs.readFileSync(path + '/README.md', 'utf8');
+
+        // We need all package directories
+        const packageDirectories = sfdxJson.packageDirectories;
+
+        packageDirectories.forEach(dir => {
+            const package = dir.package;
+            const packageKey = package + '@';
+            const packageVersionKeys = [];
+            const packageVersionIds = [];
+            Object.keys(sfdxJson.packageAliases).forEach(packageVersion => {
+                if (packageVersion.startsWith(packageKey)) {
+                    packageVersionKeys.push(packageVersion);
+                    packageVersionIds.push(
+                        sfdxJson.packageAliases[packageVersion]
+                    );
+                }
+            });
+            const newPackageVersionId =
+                packageVersionIds[packageVersionIds.length - 1];
+            // Lets pop things so that we know what to delete
+            packageVersionKeys.pop();
+            packageVersionIds.pop();
+            // Removing all no longer needed package version keys
+            packageVersionKeys.forEach(version => {
+                delete sfdxJson.packageAliases[version];
+            });
+            // And we need now all IDs to replace them in the README
+            packageVersionIds.forEach(id => {
+                // If you want to use Regex to identify package version id => /04t(.*)\)/
+                readmeContent = readmeContent.replace(id, newPackageVersionId);
+            });
+        });
+
+        // Writing back potential changes to the sfdx-project.json file...
+        fs.writeFileSync(
+            './sfdx-project.json',
+            JSON.stringify(sfdxJson, null, 4).concat('\n'),
+            'utf8'
+        );
+
+        // And finally we're updating the README.
+        fs.writeFileSync(path + '/README.md', readmeContent, 'utf8');
+
+        core.setOutput('isSuccess', true);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
